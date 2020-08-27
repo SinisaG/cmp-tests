994bbd7b7b9c0e2137de763b59bdae51160a5d95
sinisag <sinisa@grubor.si>
2020-08-27 15:39:43 +0200

    :wrench:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <h1>CPM</h1>
-    <div id="CookieDeclaration"></div>
+    <p>
+      This app displayes and integrates multiple consent platform managers to
+      test differences between them.
+    </p>
   </div>
 </template>
+
 <script>
 export default {
   name: 'Home',
-  mounted() {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://consent.cookiebot.com/d83dbe81-af22-45e4-9b02-e87100e76d4e/cd.js';
-    document.head.appendChild(script);
-  },
 };
 </script>
 
