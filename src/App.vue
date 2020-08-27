e298769eaf13ff98f249ffaf8408cce82b29e177
sinisag <sinisa@grubor.si>
2020-08-27 15:41:12 +0200

    :wrench:

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ body, html {
   padding: 0;
   margin: 0;
 }
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
